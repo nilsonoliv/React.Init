0ba5b2e3b5f1583bbb7b26bdcec2d3ffff7dfa4b
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2024-05-13 22:04:17 -0300

    componente soma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import { soma } from './js/Soma';
 /*import { hello } from './js/hello';
 import tick from './js/tick';
 import { welcome } from './js/welcome';
@@ -8,10 +9,15 @@ import React from 'react';*/
 
 
 function App() {
-  
+  var somaVariavel = soma(12,3)
 
-  return(
+  return (
+    <div>
     <h1>Hello ReactJs</h1>
+    <p>{somaVariavel}</p>
+
+    </div>
+    
  )
 
   /*return (
--- src/js/Soma.js
+++ src/js/Soma.js
@@ -1,1 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
+export function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+}
