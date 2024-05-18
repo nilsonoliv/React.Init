8f072a106f2b2a559b18ef731db404062fde5337
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2024-05-18 20:25:58 -0300

    comentando no action app3

--- app3/src/actions/index.js
+++ app3/src/actions/index.js
@@ -1,3 +1,8 @@
+/*Essa função simples é chamada de "Action Creator" e é responsável por criar e retornar 
+um objeto "action", que, ao ser disparado, irá comunicar ao Reducer 
+que o type é "CLICK_UPDATE_VALUE", além do valor "newValue: value" 
+que deverá ser atualizado na Store.*/
+
 
 export const clickButton = value => ({ 
     type: 'CLICK_UPDATE_VALUE', 
