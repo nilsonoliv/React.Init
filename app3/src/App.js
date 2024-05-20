3652a835bbf7045ee067e3235cce5800b9933754
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2024-05-20 17:41:17 -0300

    comentario

--- app3/src/App.js
+++ app3/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
+/*O codigo do curso esta desatualizado*/
 
  class App extends Component() {
   render(){
