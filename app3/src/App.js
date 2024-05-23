4ae4dd9f2343122f5c655f2e71aa986334e1e1a7
Reinilson Oliveira <nilsonespiritosant@gmail.com>
2024-05-23 16:22:17 -0300

    concluindo a base

--- app3/src/App.js
+++ app3/src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {clickButton} from './actions'
+
 /*O codigo do curso esta desatualizado*/
 
  class App extends Component() {
+  state = {
+    inputValue: ''
+  }
+  inputChange = event =>{
+    this.setState({
+      inputValue: event.target.value 
+    })
+  }
+
   render(){
-    const {newValue} = this.props; //constante local chamada "newValue" (novo valor), que irá receber os dados de Props que serão mapeados com os dados da Store.
+    const {
+      clickButton,
+      newValue
+    } = this.props; //constante local chamada "newValue" (novo valor), que irá receber os dados de Props que serão mapeados com os dados da Store.
+    
+    const {inputValue} = this.state;
+
     return(
     <div className="App" style={{ paddingTop: '10px' }}>     
     <h2>App3: React com Redux</h2> 
-    <input type='text' /> 
-    <button>Enviar</button> 
+    <input type='text' onChange={this.inputChange} value={inputValue}/> 
+    <button onClick={() => clickButton(inputValue)}>Enviar</button> 
     <h3>{newValue}</h3> 
     </div> 
     )
@@ -20,7 +38,13 @@ import { connect } from 'react-redux';
 const mapStateToProps = store => ({       //. O método mapStateToProps transforma um trecho 
   newValue: store.clickState.newValue     //do estado da Store em uma prop utilizável pelo componente, com o nome newValue.
 });
+const mapDispatchToProps = Dispatch =>
+  bindActionCreators({clickButton }, Dispatch);
 
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
 /*Realizamos, ao mesmo tempo, a exportação da aplicação (componente "App"), 
 agora recebida como parâmetro da função "connect", que também recebe como parâmetro o mapeamento da Store.*/
+
+//OBJETIVO DO CÓDIGO
+/*Pronto! Se você seguiu esses passos corretamente, ao digitar um texto e acionar o botão, o valor do input será direcionado para 
+o componente "h3" e exibido logo abaixo, conforme imagem a seguir.*/
